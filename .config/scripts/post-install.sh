a823e2b21d773f1ef8d6d820c388ccec9fee3a68
Ashleigh Wilson <ashleigh.wilson85@icloud.com>
2022-06-12 06:49:14 +0100

    Run chsh as sudo on arch post install.

--- .config/scripts/post-install.sh
+++ .config/scripts/post-install.sh
@@ -99,7 +99,7 @@ paru -S --needed --noconfirm ${SOFTWARE_GNOME[@]} ${SOFTWARE[@]}
 
 mkdir $HOME/.local/share/backgrounds/
 cp $HOME/.config/wallpapers/fox-forest.jpg $HOME/.local/share/backgrounds/2022-06-10-19-33-29-fox-forest.jpg
-chsh -s /usr/bin/zsh
+sudo chsh -s /usr/bin/zsh $USER
 rm -rf $HOME/.bash*
 
 # enable services
