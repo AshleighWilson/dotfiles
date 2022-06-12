268760477c8e2df2d7c277b4bf9aef9af393a41d
Ashleigh Wilson <ashleigh.wilson85@icloud.com>
2022-06-12 06:35:00 +0100

    Attempt to prevent error with which-key on fresh install.

--- .config/nvim/lua/plugins/which-key.lua
+++ .config/nvim/lua/plugins/which-key.lua
@@ -4,30 +4,31 @@ PACKER.use {
 	config = function()
 		require("which-key").setup {
 		}
+
+		local wk = require("which-key")
+		wk.register({
+			["<leader>b"] = { name = "+buffer" },
+			["<leader>bc"] = {"<cmd>bd<cr>", "Close Buffer" },
+
+			["<leader>f"] = { name = "+file" },
+			["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find File" },
+			["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
+			["<leader>fn"] = { "<cmd>enew<cr>", "New File" },
+			["<leader>ft"] = { "<cmd>NvimTreeFindFile<cr>", "View Filesystem Tree" },
+
+			["<leader>n"] = { name = "+neovim" },
+			["<leader>np"] = { name = "+packer" },
+			["<leader>nps"] = { "<cmd>PackerSync<cr>", "Packer Sync" },
+
+			["<leader>c"] = { name = "+code" },
+			["<leader>cb"] = { "<cmd>make<cr>", "Build" },
+			["<leader>cu"] = { "<cmd>make all upload<cr>", "Build and Upload" },
+		},
+		{
+		})
 	end
 }
 
-wk = require("which-key")
-wk.register({
-	["<leader>b"] = { name = "+buffer" },
-	["<leader>bc"] = {"<cmd>bd<cr>", "Close Buffer" },
-
-	["<leader>f"] = { name = "+file" },
-	["<leader>ff"] = { "<cmd>Telescope find_files<cr>", "Find File" },
-	["<leader>fr"] = { "<cmd>Telescope oldfiles<cr>", "Open Recent File" },
-	["<leader>fn"] = { "<cmd>enew<cr>", "New File" },
-	["<leader>ft"] = { "<cmd>NvimTreeFindFile<cr>", "View Filesystem Tree" },
-
-	["<leader>n"] = { name = "+neovim" },
-	["<leader>np"] = { name = "+packer" },
-	["<leader>nps"] = { "<cmd>PackerSync<cr>", "Packer Sync" },
-
-	["<leader>c"] = { name = "+code" },
-	["<leader>cb"] = { "<cmd>make<cr>", "Build" },
-	["<leader>cu"] = { "<cmd>make all upload<cr>", "Build and Upload" },
-},
-{
-})
 
 -- Orgmode
 -- https://github.com/nvim-orgmode/orgmode
