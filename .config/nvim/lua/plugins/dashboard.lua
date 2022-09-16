d3cc6c878c9b611af60c862a9070dd59c9d6ee22
Ashleigh Wilson <ashleigh.wilson85@icloud.com>
2022-09-16 18:17:34 +0100

    Added custom dashboard config for neovim.

--- .config/nvim/lua/plugins/dashboard.lua
+++ .config/nvim/lua/plugins/dashboard.lua
@@ -4,5 +4,71 @@ PACKER.use {
 	requires = {},
 	config = function()
 		vim.g.dashboard_default_executive = "telescope"
-	end
+	end,
+
 }
+
+local dashboard = require('dashboard')
+
+dashboard.custom_header = {
+  "                                                       ",
+  "                                                       ",
+  "                                                       ",
+  " ███╗   ██╗ ███████╗ ██████╗  ██╗   ██╗ ██╗ ███╗   ███╗",
+  " ████╗  ██║ ██╔════╝██╔═══██╗ ██║   ██║ ██║ ████╗ ████║",
+  " ██╔██╗ ██║ █████╗  ██║   ██║ ██║   ██║ ██║ ██╔████╔██║",
+  " ██║╚██╗██║ ██╔══╝  ██║   ██║ ╚██╗ ██╔╝ ██║ ██║╚██╔╝██║",
+  " ██║ ╚████║ ███████╗╚██████╔╝  ╚████╔╝  ██║ ██║ ╚═╝ ██║",
+  " ╚═╝  ╚═══╝ ╚══════╝ ╚═════╝    ╚═══╝   ╚═╝ ╚═╝     ╚═╝",
+  "                                                       ",
+  "                                                       ",
+  "                                                       ",
+  "                                                       ",
+}
+
+dashboard.custom_center = {
+  {
+    icon = "  ",
+    desc = "Find  File                              ",
+    action = "Telescope find_files find_command=rg,--hidden,--files",
+    shortcut = "<Leader> ff",
+  },
+  {
+    icon = "  ",
+    desc = "Recently opened files                   ",
+    action = "Telescope oldfiles",
+    shortcut = "<Leader> fr",
+  },
+  {
+    icon = "  ",
+    desc = "Open project                            ",
+    action = "Telescope project",
+    shortcut = "<Leader> cp",
+  },
+  {
+    icon = "  ",
+    desc = "Open Nvim config                        ",
+    action = "tabnew $MYVIMRC | tcd %:p:h",
+    shortcut = "<Leader> ev",
+  },
+  {
+    icon = "  ",
+    desc = "New file                                ",
+    action = "enew",
+    shortcut = "n          ",
+  },
+  {
+    icon = "  ",
+    desc = "Quit Nvim                               ",
+    action = "qa",
+    shortcut = "q          ",
+  },
+}
+
+vim.cmd([[
+  augroup dashboard_enter
+    au!
+    autocmd FileType dashboard nnoremap <buffer> q :qa<CR>
+    autocmd FileType dashboard nnoremap <buffer> n :enew<CR>
+  augroup END
+]])
