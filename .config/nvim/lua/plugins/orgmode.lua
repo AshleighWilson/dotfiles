8cfb35edaf045bbc9c24b61f2283104c8bda30ea
Ashleigh Wilson <ashleigh.wilson85@icloud.com>
2022-10-23 16:16:55 +0100

    Enable treesitter for org mode and updated org directories.

--- .config/nvim/lua/plugins/orgmode.lua
+++ .config/nvim/lua/plugins/orgmode.lua
@@ -9,7 +9,7 @@ PACKER.use {
 				disable_all = true
 			}
 		}
-		
+
 		require('orgmode').setup_ts_grammar()
 
 		-- Tree-sitter configuration
@@ -18,15 +18,18 @@ PACKER.use {
 			-- If TS highlights are not enabled at all, or disabled via `disable` prop, highlighting will fallback to default Vim syntax highlighting
 			highlight = {
 				enable = true,
-				disable = {'org'}, -- Remove this to use TS highlighter for some of the highlights (Experimental)
 				additional_vim_regex_highlighting = {'org'}, -- Required since TS highlighter doesn't support all syntax features (conceal)
 			},
 			ensure_installed = {'org'}, -- Or run :TSUpdate org
 		}
 
 		require('orgmode').setup({
-			org_agenda_files = {'~/Dropbox/org/*', '~/my-orgs/**/*'},
-			org_default_notes_file = '~/Dropbox/org/refile.org'
+			org_agenda_files = {'~/Nextcloud/Documents/Org/*'},
+			org_default_notes_file = '~/Nextcloud/Documents/Org/refile.org',
+			org_todo_keywords = {'TODO', 'NEXT', '|', 'DONE' },
+			mappings = {
+				disable_all = false,
+			},
 		})
 
 		require("org-bullets").setup {
