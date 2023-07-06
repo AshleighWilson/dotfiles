0d45f11862cd90736d6abbbd5e5837d9d6bdda57
Ashleigh <ashleigh.wilson85@icloud.com>
2023-07-06 19:14:17 +0100

    Added shutdown and lockscreen scripts.

---
+++ .config/scripts/shutdown.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+OPTIONS=(
+	lock
+	suspend
+	hibernate
+	logout
+	reboot
+	shutdown
+)
+
+choice=$(printf "%s\n" "${OPTIONS[@]}" | rofi -dmenu -i -p "shutdown " -theme-str 'entry { enabled: false;}')
+
+if [ "$choice" == "lock" ]; then
+	betterlockscreen -l
+elif [ "$choice" == "suspend" ]; then
+	systemctl suspend
+elif [ "$choice" == "hibernate" ]; then
+	systemctl hibernate
+elif [ "$choice" == "logout" ]; then
+	awesome-client 'awesome.quit()'
+elif [ "$choice" == "reboot" ]; then
+	systemctl reboot
+elif [ "$choice" == "shutdown" ]; then
+	systemctl poweroff
+else
+	echo "Exiting.."
+fi
